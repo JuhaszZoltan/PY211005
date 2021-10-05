2489779c8b7ee9118eaf54bd67139f58754beaa6
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2021-10-05 11:29:00 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,55 @@
+# valtozo = 2
+# db = 0
+
+# while valtozo < 1000000000:
+#     # print(valtozo)
+#     db = db + 1
+#     valtozo = valtozo * 2
+
+# print(f'összesen {db} négyzetreemelést végeztünk el')
+
+# for i in range(6):
+#     print(i)
+
+import math
+import random
+
+# # feladatok 2/1
+# for i in range(10):
+#     print(f'{i}. Juhasz Zoltan')
+
+#feladatok 2/2
+
+# print('Szia', end=' ')
+# print('Zoli')
+
+# n = int(input('Írj be egy számot: '))
+# szoveg = input('Írj be egy szöveget: ')
+
+# for i in range(n):
+#     print(szoveg, end= ' ')
+
+import time
+import os
+import winsound
+
+# n = 10
+# for i in range(10):
+#     print(n)
+#     time.sleep(1)
+#     winsound.Beep(2500, 1000)
+#     n -= 1
+# os.system('cls')
+
+# n = n - 1 ||| n -= 1
+# n = n + 1 ||| n += 1
+
+# for i in  range(20):
+#     print(i * ' ')
+#     print('Zoli')
+#     time.sleep(.5)
+#     os.system('cls')
+
+
+
+
